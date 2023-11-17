0bc7409f73a5ae0b8954e9d79275390e0ad26ce9
Jay <jerry.sirisavath@go.minneapolis.edu>
2023-11-16 21:19:48 -0600

    Added video class model and migrations to db

--- video_collection/admin.py
+++ video_collection/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Video
+
 # Register your models here.
+
+# Video class model
+admin.site.register(Video)
--- video_collection/models.py
+++ video_collection/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+# What goes in a video class? 
+# Name, YTVidURL, and optional Note
+
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes= models.TextField(blank=True, null=True)
+    
+    # Formatted way to display the information strings
+    # Truncate to the first 200 letters
+    def __str__(self):
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
